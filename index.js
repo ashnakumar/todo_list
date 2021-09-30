28f6f4435df2a2cf00f7fc26ba47770da34121cb
samaranand <samarss2015@gmail.com>
2021-09-30 22:39:05 +0530

    hack 2021

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ app.set('view engine', 'hbs');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 var tasks = ['Complete todo list project'];
+var tmpArr = [];
+
 
 function check(idx){return (idx < tasks.length && idx >= 0);}
 
@@ -11,12 +13,14 @@ app.get('/', function(req, res){res.render(__dirname + '/index.hbs', {tasks});})
 
 app.post('/add', function(req, res){
     tasks.push(req.body.task);
+    tmpArr.push(tasks);
     res.redirect('/');
 });
 
 app.post('/del', function(req, res){
     const result = check(req.body.task);
     if(result)tasks.splice(req.body.task, 1);
+    else tmpArr.push(result);
     res.redirect('/');
 });
 
