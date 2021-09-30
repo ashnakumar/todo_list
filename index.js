21a990e4de38f0da66b8b3295096e506ee510e2f
PatelManav <58802170+PatelManav@users.noreply.github.com>
2021-09-30 22:30:33 +0530

    added proper naming convention

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ app.use(express.urlencoded({extended: true}));
 var tasks = ['Complete todo list project'];
 
 function check(idx){return (idx < tasks.length && idx >= 0);}
-
+const ADD_ROUTE = "/add", DEL_ROUTE = "/del";
 app.get('/', function(req, res){res.render(__dirname + '/index.hbs', {tasks});});
 
-app.post('/add', function(req, res){
+app.post(ADD_ROUTE, function(req, res){
     tasks.push(req.body.task);
     res.redirect('/');
 });
 
-app.post('/del', function(req, res){
+app.post(DEL_ROUTE, function(req, res){
     const result = check(req.body.task);
     if(result)tasks.splice(req.body.task, 1);
     res.redirect('/');
